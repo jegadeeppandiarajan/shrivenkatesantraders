b0591ab6e2f4af67fad0072c4441a886320f8fd6
jegadeep <jegadeeppandiarajan2003@gmail.com>
2026-03-11 10:45:38 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://shrivenkatesantraders.vercel.app/",
         changeOrigin: true,
         secure: false,
       },
